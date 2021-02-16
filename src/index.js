154a6d0c1277214851d002eed2ea4c0f9d282ead
Marat Maksumov <maksumov@users.noreply.github.com>
2021-02-16 22:30:43 +0300

    feat: implement correct brackets sequence checking
    I didn't test the solution in the node 10 environment...

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+const finalReducer = (result) => (result.stack.length ? false : result.res);
+
+module.exports = check = (str, bracketsConfig) => {
+    const config = {};
+    bracketsConfig.forEach((pair) => (config[pair[0]] = pair[1]));
+
+    return finalReducer(
+        str.split("").reduce(
+            (acc, char) => ({
+                res:
+                    acc.res &&
+                    ((acc.stack.length > 0 &&
+                        acc.stack[acc.stack.length - 1] === char) ||
+                        Object.keys(config).includes(char)),
+                stack:
+                    acc.stack.length > 0 &&
+                    acc.stack[acc.stack.length - 1] === char
+                        ? acc.stack.slice(0, acc.stack.length - 1)
+                        : Object.keys(config).includes(char)
+                        ? acc.stack.concat([config[char]])
+                        : acc.stack,
+            }),
+            { res: true, stack: [] }
+        )
+    );
+};
